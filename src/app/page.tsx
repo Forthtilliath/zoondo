1b0c7a6b930b08897e25552dc6f3e84349264166
Coding-Jarl <senvisage@gmail.com>
2023-09-11 22:54:08 +0200

    feat: remove sample content + add board

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,47 +2,37 @@ import Board from '@/components/Board'
 import Square from '@/components/Square'
 
 export default function Home() {
-  //let board: Game.Board = Array<Game.Square>
+  let board: Array<Game.Square> = []
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 6; j++) {
+      const square = {
+        x: i,
+        y: j,
+      }
+      board.push(square)
+    }
+  }
+
+  let sampleCard: Game.Card = {
+    slug: 'valhallacode',
+    name: 'ValhallaCode',
+    corners: [3, 3, '*', 1],
+    type: 'emblem',
+    value: 10,
+    moves: [],
+  }
 
   return (
     <>
       <Board>
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
-        <Square />
+        {board.map((sq) => {
+          if (sq.x < 2)
+            return <Square key={`${sq.x};${sq.y}`} card={sampleCard} />
+          if (sq.x > 3)
+            return <Square key={`${sq.x};${sq.y}`} card={sampleCard} isOwned />
+
+          return <Square key={`${sq.x};${sq.y}`} />
+        })}
       </Board>
     </>
   )
