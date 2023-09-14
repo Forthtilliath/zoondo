038fcf772b08dbbf2f00ae1f069be2fc9452c214
Forthtilliath <forth@live.fr>
2023-09-14 17:04:47 +0200

    reafactor(types): upgrade type Card

--- src/types/game.d.ts
+++ src/types/game.d.ts
@@ -10,13 +10,29 @@ declare global {
       card?: Card
     }
 
+    type Corner = number | '*'
+    type Move = [X: number, Y: number][]
+    type Type =
+      | 'Chief'
+      | 'Priest'
+      | 'Monster'
+      | 'Elite'
+      | 'Soldier'
+      | 'Emblem'
+      | 'Asset' // ???
+    // Hero ???
+
     type Card = {
       slug: string
       name: string
-      type: string
-      corners: Array<number | '*'>
+      type: Type
+      corners: [NW: Corner, NE: Corner, SE: Corner, SW: Corner]
       value: number
-      moves: Array
+      moves: Array<Move>
+      variants?: {
+        corners?: Card['corners']
+        moves?: Card['moves']
+      }
     }
   }
 }
