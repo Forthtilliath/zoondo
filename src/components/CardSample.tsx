5c7f712e283b69a1956d6e8daa996860d199a7dc
Coding-Jarl <senvisage@gmail.com>
2023-09-27 15:30:26 +0200

    feat: draw lines in CardSample to show moves abilities

--- src/components/CardSample.tsx
+++ src/components/CardSample.tsx
@@ -15,6 +15,8 @@ export default function CardSample({
 }: Props) {
   let squares = utils.generatePositions(-2, 2)
 
+  console.log(utils.movesToTransitions(moves))
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.card}>
@@ -26,10 +28,27 @@ export default function CardSample({
           {squares.map((sq) => {
             const cssClasses = clsx(styles.square, {
               [styles.origin]: sq.x === 0 && sq.y === 0,
-              [styles.move]: moves.some(([x, y]) => x === sq.x && y === sq.y),
+              [styles.move]: moves
+                .flat()
+                .some(
+                  ([x, y]) => x === sq.x && y === sq.y && (x !== 0 || y !== 0)
+                ),
             })
             return <div key={`${sq.x}-${sq.y}`} className={cssClasses} />
           })}
+          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            {utils.movesToTransitions(moves).map(([src, dst]) => (
+              <line
+                key={`${src.x}-${src.y}-${dst.x}-${dst.y}-`}
+                x1={src.x + 50}
+                y1={src.y + 50}
+                x2={dst.x + 50}
+                y2={dst.y + 50}
+                stroke="black"
+                strokeWidth="2"
+              />
+            ))}
+          </svg>
         </div>
         <div className={styles.type}>
           <Image
