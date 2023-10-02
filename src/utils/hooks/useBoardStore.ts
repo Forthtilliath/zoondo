a60508f3726286ae7cd99eb089c29f85cac6176b
Coding-Jarl <senvisage@gmail.com>
2023-10-03 00:07:01 +0200

    feat: upgrade zustand store to be able to move cards

--- src/utils/hooks/useBoardStore.ts
+++ src/utils/hooks/useBoardStore.ts
@@ -1,26 +1,45 @@
 import { create } from 'zustand'
-import { availableCards } from '../data/cards'
+import { devtools } from 'zustand/middleware'
+import { produce } from 'immer'
+import { availableCards, cardsOnBoard } from '../data/cards'
 
 interface BoardState {
   cardFocus: Game.Card | null
   cardsOnBoard: Game.CardWithPosition[]
   setCardFocus: (card: Game.Card | null) => void
+  moveCard: (card: Game.CardWithPosition, pos: Position) => void
 }
-export const useBoardStore = create<BoardState>()((set) => ({
-  cardFocus: null,
-  cardsOnBoard: [
-    { ...availableCards['cloboulon'], x: 0, y: 1, isOwned: true },
-    { ...availableCards['gold-hure'], x: 1, y: 1, isOwned: true },
-    { ...availableCards['bouclefeuille'], x: 2, y: 1, isOwned: true },
-    { ...availableCards['sacrechene'], x: 3, y: 1, isOwned: true },
-    { ...availableCards['grognard:0'], x: 4, y: 1, isOwned: true },
-    { ...availableCards['grognard:1'], x: 5, y: 1, isOwned: true },
-    { ...availableCards['cloboulon'], x: 0, y: 4, isOwned: false },
-    { ...availableCards['cloboulon'], x: 1, y: 4, isOwned: false },
-    { ...availableCards['cloboulon'], x: 2, y: 4, isOwned: false },
-    { ...availableCards['cloboulon'], x: 3, y: 4, isOwned: false },
-    { ...availableCards['cloboulon'], x: 4, y: 4, isOwned: false },
-    { ...availableCards['cloboulon'], x: 5, y: 4, isOwned: false },
-  ] as Game.CardWithPosition[],
-  setCardFocus: (card) => set(() => ({ cardFocus: card })),
-}))
+export const useBoardStore = create<BoardState>()(
+  devtools((set) => ({
+    cardFocus: null,
+    cardsOnBoard: [
+      { ...availableCards['cloboulon'], x: 0, y: 1, isOwned: true },
+      { ...availableCards['gold-hure'], x: 1, y: 1, isOwned: true },
+      { ...availableCards['bouclefeuille'], x: 2, y: 1, isOwned: true },
+      { ...availableCards['sacrechene'], x: 3, y: 1, isOwned: true },
+      { ...availableCards['grognard:0'], x: 4, y: 1, isOwned: true },
+      { ...availableCards['grognard:1'], x: 5, y: 1, isOwned: true },
+      { ...availableCards['cloboulon'], x: 0, y: 4, isOwned: false },
+      { ...availableCards['cloboulon'], x: 1, y: 4, isOwned: false },
+      { ...availableCards['cloboulon'], x: 2, y: 4, isOwned: false },
+      { ...availableCards['cloboulon'], x: 3, y: 4, isOwned: false },
+      { ...availableCards['cloboulon'], x: 4, y: 4, isOwned: false },
+      { ...availableCards['cloboulon'], x: 5, y: 4, isOwned: false },
+    ] as Game.CardWithPosition[],
+    setCardFocus: (card) => set(() => ({ cardFocus: card })),
+    moveCard: (card, pos) => {
+      const foundCard = cardsOnBoard.findIndex(
+        (candidate) => candidate.x === card.x && candidate.y === card.y
+      )
+      set(
+        produce((state) => {
+          console.log(state.cardsOnBoard[foundCard])
+
+          state.cardsOnBoard[foundCard] = { ...card, ...pos }
+
+          console.log(state.cardsOnBoard[foundCard])
+        })
+      )
+    },
+  }))
+)
