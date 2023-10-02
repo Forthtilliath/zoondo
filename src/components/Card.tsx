baec73332b0aaf3b7e51b3ef8143e143fac46141
Coding-Jarl <senvisage@gmail.com>
2023-10-03 00:05:22 +0200

    feat: make Card draggable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,17 +4,26 @@ import back from '@/assets/card-back-board.png'
 import backAdv from '@/assets/card-back-board-alt.png'
 
 import styles from '@/styles/components/Card.module.scss'
+import { useDrag } from 'react-dnd'
 
 type Props = Game.CardOwned
 
-export default function Card({ name, isOwned }: Props) {
+export default function Card({ name, isOwned, ...otherProps }: Props) {
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'CLOBOULON',
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+    item: { name, isOwned, ...otherProps },
+  }))
+
   const cssClasses = clsx(
     styles.wrapper,
     isOwned ? styles.isOwned : styles.adversary
   )
 
   return (
-    <div className={cssClasses}>
+    <div className={cssClasses} ref={drag}>
       <Image src={isOwned ? back : backAdv} alt="Hidden Card" />
       <p>{name}</p>
     </div>
